878ce55d56b585d546423cb54407dde47dd888f3
angsylvest <sylve057@umn.edu>
2024-04-09 17:14:02 -0500

    supervisor work to update occupancy grid

--- webots_code/controllers/lifelong_a_supervisor/lifelong_a_supervisor.py
+++ webots_code/controllers/lifelong_a_supervisor/lifelong_a_supervisor.py
@@ -1,32 +1,107 @@
-"""lifelong_a_supervisor controller."""
+from controller import Supervisor, Node, Keyboard, Emitter, Receiver, Field
+import math 
+import random
+import ast
+from math import pi
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+# set-up robot 
+TIME_STEP = 32
+robot = Supervisor()  # create Supervisor instance
+emitter = robot.getDevice("emitter")
+emitter.setChannel(2)
+receiver = robot.getDevice("receiver") 
+receiver.enable(TIME_STEP)
+receiver.setChannel(1) 
 
-# create the Robot instance.
-robot = Robot()
+trials = 1
+simulation_time = 600 
+other_obstacle_ids = []
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+def message_listener(time_step):
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+    if receiver.getQueueLength()>0:
+        # message = receiver.getData().decode('utf-8')
+        # print('supervisor msgs --', message) 
+        message = receiver.getString()
+        
+        if message: 
+            receiver.nextPacket() 
+        else: 
+            receiver.nextPacket() 
+            
+    
+# runs simulation for designated amount of time 
+def run_seconds(t):
+    
+    n = TIME_STEP / 1000*32 # convert ms to s 
+    start = robot.getTime()
+    new_t = round(t, 1)
+    
+    while robot.step(TIME_STEP) != -1:
+        # run robot simulation for 30 seconds (if t = 30)
+        increments = TIME_STEP / 1000
+        
+        if robot.getTime() - start > new_t: 
+            break 
+    return 
+    
+def set_agent_up(start): 
+    x, y = start 
+    node = robot.getFromDef('main-e-puck')
+    translation_field = node.getField('translation')
+    new_value = [x, y, 0]
+    translation_field.setSFVec3f(new_value)
+    
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+def create_goal_msg(start = (0,0), goal = (0.3,0.3)):
+    # update agent pos here 
+    set_agent_up(start)
+    
+    msg = "goal_update:"
+    startx, starty = start 
+    goalx, goaly = goal 
+    
+    msg += str(startx) + "-" + str(starty) + "-"
+    msg += str(goalx) +  "-" + str(goaly)
+    
+    print(f'attempting to send goal to controller: {msg}')
+    
+    emitter.send(msg)
 
-    # Process sensor data here.
+def find_obstacles(curr_agent, obstacle_poses, range):
+    poses_within_range = []
+    for a in other_obstacle_ids:
+        node = robot.getFromDef(str(a))
+        pose = node.getPosition()
+        distance = ((pose[0] - curr_agent[0])**2 + (pose[1] - curr_agent[1])**2)**0.5
+        if distance <= range:
+            poses_within_range.append(pose)
+    return poses_within_range     
+           
+def run_optimization():
+    
+    for i in range(trials): 
+        print('beginning new trial', i)
+        
+        # Update start and goal pose for given agent 
+        create_goal_msg() # TODO: enable abiltiy to add custom start and goal pose 
+        
+        # time limit to run 
+        run_seconds(simulation_time) 
+            
+        # include metric info here 
+        msg = 'trial' + str(i)
+        emitter.send(msg) 
+        prev_msg = msg
+        
+    # reset env here 
+         
+    return 
+  
+def main(): 
+    run_optimization()
+    # save_progress()
+         
+main()
+                    
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
